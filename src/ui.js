9823d04cd585a39d3434135fc5aff99b4e876438
jgi <public-vs2015@gissehel.org>
2019-11-02 10:14:37 +0100

    REFACT : Simply creation/updating of grouped button elements in ui

--- src/ui.js
+++ src/ui.js
@@ -30,7 +30,7 @@ class Ui {
         const resultPanel = this._createPanel(workspace, ['ui', 'segment', 'resultPanel']);
 
         const dataNamePanel = this._createPanel(controlPanel, ['field', 'dataNamePanel']);
-        
+
         const prefixSizeCountPanel = this._createPanel(controlPanel, ['two', 'fields', 'prefixSizeCountPanel']);
         const prefixSizePanel = this._createPanel(prefixSizeCountPanel, ['field', 'prefixSizePanel']);
         const countPanel = this._createPanel(prefixSizeCountPanel, ['field', 'countPanel']);
@@ -50,79 +50,54 @@ class Ui {
         this._createLabel(minWordPanel, 'Minimum word size', ['minSize']);
         this._createLabel(maxWordPanel, 'Maximum word size', ['maxSize']);
 
-
         /**
-         * @type {Object.<string, HTMLElement>}
+         * @type {Object.<string, Object.<string, HTMLElement>>}
          */
-        this._dataNames = {};
+        this._buttonElementsByTypeByValue = {};
 
-        /**
-         * @type {Object.<number, HTMLElement>}
-         */
-        this._prefixLens = {};
         const prefixLen = [2, 3, 4, 5, 6];
-
-        /**
-         * @type {Object.<number, HTMLElement>}
-         */
-        this._counts = {};
         const counts = [2, 5, 10, 15, 20];
-
-        /**
-         * @type {Object.<number, HTMLElement>}
-         */
-        this._minSizes = {};
         const minSizes = [5, 6, 7, 8, 20, 30];
-
-        /**
-         * @type {Object.<number, HTMLElement>}
-         */
-        this._maxSizes = {};
         const maxSizes = [10, 12, 14, 16, 18, 20, 45, 85];
 
         this._createButtons(
             dataNamePanel,
-            this._dataNames,
+            'dataName',
             Object.keys(this._data),
             (dataName) => this._data[dataName].name,
-            (dataName) => this.onDataNameClicked(dataName),
-            ['dataName']
+            (dataName) => this.onDataNameClicked(dataName)
         );
 
         this._createButtons(
             prefixSizePanel,
-            this._prefixLens,
+            'prefixLen',
             prefixLen,
             (prefixLen) => `${prefixLen}`,
-            (prefixLen) => this.onPrefixLenClicked(prefixLen),
-            ['prefixLen']
+            (prefixLen) => this.onPrefixLenClicked(prefixLen)
         );
 
         this._createButtons(
             countPanel,
-            this._counts,
+            'count',
             counts,
             (count) => `${count}`,
-            (count) => this.onCountClicked(count),
-            ['count']
+            (count) => this.onCountClicked(count)
         );
 
         this._createButtons(
             minWordPanel,
-            this._minSizes,
+            'minSize',
             minSizes,
             (minSize) => `${minSize}`,
-            (minSize) => this.onMinSizeClicked(minSize),
-            ['minSize']
+            (minSize) => this.onMinSizeClicked(minSize)
         );
 
         this._createButtons(
             maxWordPanel,
-            this._maxSizes,
+            'maxSize',
             maxSizes,
             (maxSize) => `${maxSize}`,
-            (maxSize) => this.onMaxSizeClicked(maxSize),
-            ['maxSize']
+            (maxSize) => this.onMaxSizeClicked(maxSize)
         );
 
         this._state.onControlDataChanged.add(({ state, changed }) => this.onControlDataChanged(state, changed), { state: this._state });
@@ -150,16 +125,23 @@ class Ui {
     /**
      * 
      * @param {HTMLElement} parent 
-     * @param {Object.<T, HTMLElement>} elements 
+     * @param {string} buttonType
      * @param {T[]} elementRefs 
      * @param {(ref:T)=>string} getName 
      * @param {(ref:T)=>void} onClick 
      * @param {string[]} classes 
      * @template T
      */
-    _createButtons(parent, elements, elementRefs, getName, onClick, classes) {
+    _createButtons(parent, buttonType, elementRefs, getName, onClick, classes=null) {
+        classes = classes || [];
+        classes.push(buttonType);
+
+        if (this._buttonElementsByTypeByValue[buttonType] === undefined) {
+            this._buttonElementsByTypeByValue[buttonType] = {};
+        }
+
         elementRefs.forEach((ref) => {
-            elements[ref] = this._dom.createElement({
+            this._buttonElementsByTypeByValue[buttonType][ref] = this._dom.createElement({
                 parent,
                 name: 'button',
                 text: getName(ref),
@@ -267,13 +249,12 @@ class Ui {
 
     /**
      * 
-     * @param {Object.<T, HTMLElement>} htmlElements 
      * @param {boolean} all 
      * @param {string[]} changed 
      * @param {string} propertyName
-     * @template T
      */
-    _updateButtonsForProperty(htmlElements, all, changed, propertyName) {
+    _updateButtonsForProperty(all, changed, propertyName) {
+        const htmlElements = this._buttonElementsByTypeByValue[propertyName];
         if (all || changed.includes(propertyName)) {
             const propertyValue = `${state[propertyName]}`;
             Object.keys(htmlElements).forEach((key) => {
@@ -288,11 +269,11 @@ class Ui {
      */
     onControlDataChanged(state, changed) {
         const all = changed === undefined;
-        this._updateButtonsForProperty(this._dataNames, all, changed, 'dataName');
-        this._updateButtonsForProperty(this._prefixLens, all, changed, 'prefixLen');
-        this._updateButtonsForProperty(this._counts, all, changed, 'count');
-        this._updateButtonsForProperty(this._minSizes, all, changed, 'minSize');
-        this._updateButtonsForProperty(this._maxSizes, all, changed, 'maxSize');
+        this._updateButtonsForProperty(all, changed, 'dataName');
+        this._updateButtonsForProperty(all, changed, 'prefixLen');
+        this._updateButtonsForProperty(all, changed, 'count');
+        this._updateButtonsForProperty(all, changed, 'minSize');
+        this._updateButtonsForProperty(all, changed, 'maxSize');
     }
 
     /**
