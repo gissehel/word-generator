57cff387cd16102eca5d948c920a6f917ae0e6ea
jgi <public-vs2015@gissehel.org>
2019-11-02 10:47:55 +0100

    REFACT : Simplify/Factorrize State getter/setter

--- src/state.js
+++ src/state.js
@@ -3,19 +3,6 @@ const stateProps = ['dataName', 'prefixLen', 'count', 'minSize', 'maxSize'];
 
 class State {
     constructor() {
-        /** @type {string} */
-        this._dataName = 'fr';
-        /** @type {number} */
-        this._minSize = 6;
-        /** @type {number} */
-        this._maxSize = 16;
-        /** @type {number} */
-        this._count = 10;
-        /** @type {number} */
-        this._prefixLen = 3;
-        /** @type {string[]} */
-        this._results = [];
-
         /** @type {Ev<{state:State, changed: string[]}>} */
         this.onControlDataChanged = new Ev()
         /** @type {Ev<{state:State, result: string}>} */
@@ -24,56 +11,78 @@ class State {
         this.onResultCleaned = new Ev()
         /** @type {Ev<{hash:string}>} */
         this.onHashChanged = new Ev()
+        /** @type {string[]} */
+        this._results = [];
 
         this._hashUpdate = false;
+
+        this._properties = {};
+
+        this.dataName = 'fr';
+        this.minSize = 6;
+        this.maxSize = 16;
+        this.count = 10;
+        this.prefixLen = 3;
+
     }
 
-    get dataName() { return this._dataName; }
-    get minSize() { return this._minSize; }
-    get maxSize() { return this._maxSize; }
-    get count() { return this._count; }
-    get prefixLen() { return this._prefixLen; }
-
-    set dataName(value) {
-        if (this._dataName !== value) {
-            this._dataName = value;
-            this.onControlDataChanged.execute({ state: this, changed: ['dataName'] });
-            this.notifyHashChanged();
-        }
-    }
-    set prefixLen(value) {
-        if (this._prefixLen !== value) {
-            this._prefixLen = value * 1;
-            this.onControlDataChanged.execute({ state: this, changed: ['prefixLen'] });
-            this.notifyHashChanged();
-        }
-    }
-    set minSize(value) {
-        if (this._minSize !== value) {
-            this._minSize = value * 1;
-            this.onControlDataChanged.execute({ state: this, changed: ['minSize'] });
-            this.notifyHashChanged();
-        }
-    }
-    set maxSize(value) {
-        if (this._maxSize !== value) {
-            this._maxSize = value * 1;
-            this.onControlDataChanged.execute({ state: this, changed: ['maxSize'] });
-            this.notifyHashChanged();
-        }
-    }
-    set count(value) {
-        if (this._count !== value) {
-            this._count = value * 1;
-            this.onControlDataChanged.execute({ state: this, changed: ['count'] });
+    /**
+     * @param {string} propertyName 
+     * @param {string} value 
+     * @returns {void}
+     */
+    propertySetterString(propertyName, value) {
+        if (this._properties[propertyName] !== value) {
+            this._properties[propertyName] = value;
+            this.onControlDataChanged.execute({ state: this, changed: [propertyName] });
             this.notifyHashChanged();
         }
     }
 
+    /**
+     * @param {string} propertyName 
+     * @param {number} value 
+     * @returns {void}
+     */
+    propertySetterNumber(propertyName, value) {
+        if (this._properties[propertyName] !== value) {
+            this._properties[propertyName] = value * 1;
+            this.onControlDataChanged.execute({ state: this, changed: [propertyName] });
+            this.notifyHashChanged();
+        }
+    }
+
+    /**
+     * @param {string} propertyName 
+     * @returns {number}
+     */
+    propertyGetterNumber(propertyName) {
+        return this._properties[propertyName];
+    }
+
+    /**
+     * @param {string} propertyName 
+     * @returns {string}
+     */
+    propertyGetterString(propertyName) {
+        return this._properties[propertyName];
+    }
+
+    get dataName() { return this.propertyGetterString('dataName'); }
+    get minSize() { return this.propertyGetterNumber('minSize'); }
+    get maxSize() { return this.propertyGetterString('maxSize'); }
+    get count() { return this.propertyGetterNumber('count'); }
+    get prefixLen() { return this.propertyGetterNumber('prefixLen'); }
+
+    set dataName(value) { this.propertySetterString('dataName', value); }
+    set prefixLen(value) { this.propertySetterNumber('prefixLen', value); }
+    set minSize(value) { this.propertySetterNumber('minSize', value); }
+    set maxSize(value) { this.propertySetterNumber('maxSize', value); }
+    set count(value) { this.propertySetterNumber('count', value); }
 
     notifyHashChanged() {
-        if (! this._hashUpdate) {
-            this.onHashChanged.execute({ hash: this.hash });    
+        if (!this._hashUpdate) {
+            this.onHashChanged.execute({ hash: this.hash });
         }
     }
 
